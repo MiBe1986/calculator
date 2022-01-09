6105005ac4003eca226630466d6dbf0a844c4843
Stefanie Beck <stefanie.kanemeier@gmail.com>
2022-01-09 13:41:00 +0100

    division function with squash
    edit1
    
    edit2
    
    edit3
    
    correct

--- calc.py
+++ calc.py
@@ -4,3 +4,9 @@ def add(number_1, number_2):
 add(1,2)
 def subtract(number_1,number_2):
     return number_1-number_2
+
+def division(number_1,number_2):
+    if number_2 == 0:
+        print('division by zero is not allowed')
+    else:
+        return number_1/number_2
